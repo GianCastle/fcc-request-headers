f105e3ac8505c33bf2c0a8e04e428d2107295075
Giancarlos Castillo <castlegian@gmail.com>
2016-07-04 23:16:39 -0400

    Testing fixed ipadress

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
 const express = require('express');
-const app = express().get('/', (req, res) => {
+const app = express().enable('trust proxy').get('/', (req, res) => {
                   res.json({
-                      ipadress: req.ip,
+                      ipadress:req.headers['x-forwarded-for'] || req.connection.remoteAddress,
                       language: req.headers['accept-language'].substr(0, 5),
                       software: req.headers['user-agent'].substring(req.headers['user-agent'].indexOf('(') + 1,
                                                                     req.headers['user-agent'].indexOf(')'))
-                  });                                             
+                  });
             }).listen(process.env.PORT || 3000);
